67c5d7ab9efb40ea06d9b55454c71cb75d26667a
RAHUL DHOLE <rdhole95@gmail.com>
2025-03-17 20:07:23 +0100

    add report button

--- components/editor/EditorHeader.vue
+++ components/editor/EditorHeader.vue
@@ -9,7 +9,17 @@
           PoW IDE
         </h1>
       </div>
-      <DarkMode />
+      <div class="flex gap-4">
+        <a href="https://github.com/playonweb/pow-ide/issues" target="_blank" class="text-gray-900 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">
+          <p class="flex items-center gap-2">
+            <Icon name="material-symbols:bug-report" class="w-6 h-6" />
+            <span class="hidden lg:block">
+              Report
+            </span>
+          </p>
+        </a>
+        <DarkMode />
+      </div>
     </div>
   </header>
 </template> 
